49044ab51f193236cc5d5dbabcda32805d235c74
aklika47 <watsoniad10@gmail.com>
2026-07-02 14:24:19 +0100

    update categories seed

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => '⭐ Populaires'],
+            ['name' => '🆕 Nouveautés'],
+            ['name' => '🍽️ Menus'],
+            ['name' => '🥗 Entrées'],
+            ['name' => '🍛 Plats'],
+            ['name' => '🍔 Burgers & Sandwichs'],
+            ['name' => '🍕 Pizzas'],
+            ['name' => '🍗 Grillades'],
+            ['name' => '🍟 Accompagnements'],
+            ['name' => '🍰 Desserts'],
+            ['name' => '🥤 Boissons'],
+            ['name' => '🎁 Promotions'],
+        ];
+
+        foreach ($categories as $cat) {
+            Category::firstOrCreate(['name' => $cat['name']]);
+        }
     }
 }
