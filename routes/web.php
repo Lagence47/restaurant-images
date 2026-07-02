99f4d74e11d0e4931672bf75d5bdb782543387d6
aklika47 <watsoniad10@gmail.com>
2026-07-02 13:42:25 +0100

    add controller image, categorie

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin', function () {
+    return response()->file(public_path('admin/index.html'));
+});
+
+Route::get('/admin/{any}', function ($any) {
+    $path = public_path("admin/{$any}");
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+})->where('any', '.*');
